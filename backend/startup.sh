b1218c4e2e18a847b0c4d1a88c3ffb3d6f80ba23
Nolan <nolan.chow446@gmail.com>
2022-11-29 15:37:40 -0800

    Week 12

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/backend/wait-for-it.sh -h ${MYSQL_HOST} -p ${MYSQL_PORT} -t 30
+/bin/bash /backend/wait-for-it.sh -h ${MYSQL_HOST} -p ${MYSQL_PORT} -t 30
 envsubst < /backend/backend_template.txt > /backend/backend.conf
 /backend/.local/bin/gunicorn wsgi:app -b 0.0.0.0:5000
